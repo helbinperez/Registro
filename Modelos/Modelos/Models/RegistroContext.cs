fbc12f70420c6147a82857bab82debce0eeb6fdd
Helbin A. Perez <helbinalexander1@hotmail.com>
2020-02-16 22:24:05 -0400

    Registro de estudiante

---
+++ Modelos/Modelos/Models/RegistroContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Modelos.Models
+{
+    public partial class RegistroContext : DbContext
+    {
+        public RegistroContext()
+        {
+        }
+
+        public RegistroContext(DbContextOptions<RegistroContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudiante> Estudiante { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-RSFHOUK\\SQLEXPRESS; Database=Registro; Persist Security info= true; Integrated Security = SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudiante>(entity =>
+            {
+                entity.HasKey(e => e.IdEstudiante);
+
+                entity.Property(e => e.IdEstudiante)
+                    .HasColumnName("Id_estudiante")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Apellidos)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Carrera)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
